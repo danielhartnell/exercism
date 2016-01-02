deebd542ae0191f2a666a96303f65b52070384ca
Daniel Hartnell <daniel@Daniels-MacBook-Pro.local>
2016-01-02 09:01:28 -0800

    include gigasecond exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,22 +1,16 @@
-#!/usr/bin/env ruby
 # Hamming Exercism.io challenge
 # By Daniel Hartnell
 
 # Calculate the Hamming distance
 class Hamming
+  VERSION = 1
+
   def self.compute(segment1, segment2)
+    fail ArgumentError, 'Length error' if segment1.length != segment2.length
+
     segment1 = segment1.split('')
     segment2 = segment2.split('')
-    diff = 0
 
-    if segment1.length != segment2.length
-      fail ArgumentError, 'Segments must be the same length.'
-    end
-
-    segment1.each_with_index do |v, i|
-      diff += 1 unless v == segment2[i]
-    end
-
-    return diff
+    segment1.find_all.with_index { |v, i| v != segment2[i] }.length
   end
 end
